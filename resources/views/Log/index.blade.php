866d88d87575c1613a1a4ed755150bfa68795d6f
exnnwc <2buzezsn@gmail.com>
2016-04-19 11:24:27 -0700

    About to add Carbon

---
+++ resources/views/Log/index.blade.php
@@ -0,0 +1,42 @@
+<?php
+    $old_date = 0;
+    $old_time = 0;
+?>
+@foreach ($log_entries as $log_entry)
+    <?php
+        $date = date("m/d/y", strtotime($log_entry->when));
+        $time = date("H:i", strtotime($log_entry->when));
+    ?>
+    @if ($old_date!=$date)
+        <h1>{{ $date }}</h1>
+        <?php $old_date = $date; ?>
+    @endif
+    @if ($old_time!=$time)
+        <h3>{{ $time }}</h3>
+        <?php $old_time = $time; ?>
+    @endif
+    <div class='logEntry'>
+        <a name='entry{{ $log_entry->id }}'></a>
+        <form method="POST" action="/work-log/public/log/{{ $log_entry->id }}" class='deleteLogForm'>
+            {{ csrf_field() }}
+            {{  method_field("DELETE") }}
+            <input type='hidden' name='logEntryID' value='{{$log_entry->id}}' />
+            <input type='submit' value="X" />
+        </form>
+        @if ($log_entry->routine_id!=0)
+        <div class='logRoutine'>
+            {{ $log_entry -> routine-> type ->name}} 
+        </div>
+        @elseif ($log_entry->incident_id!=0)
+        <div class='logIncidentTitle'>Incident Report  </div> 
+        <div class='logIncident'>
+            <div class='incidentCreatedAt'>
+                Created {{$log_entry->created_at}}
+            </div>
+            <div class='incidentReport'>
+                {{ $log_entry -> incident -> report }} 
+            </div>
+        </div>
+        @endif
+    </div>
+@endforeach
---
+++ resources/views/RoutineTag.blade.php
@@ -0,0 +1,36 @@
+<div class='activeRoutineTagList'>
+    @foreach ($tags as $tag)
+        @if ($tag->routine_id == $log_entry->routine->id)
+            <span class='tags'>
+                <form method="POST" action='/work-log/public/tag/{{ $tag->id }}' class='deleteTagForm'>
+                    {{ csrf_field() }}
+                    {{ method_field("DELETE") }}
+                    <input type='hidden' name='logEntryID' value='{{$log_entry->id}}' />
+                    <input type='submit' class='textButton tagDeleteButton' value='x' />
+                </form>
+                <div class='tagName'>
+                    {{ $tag->type->name }}
+                </div>
+            </span>
+        @endif
+    @endforeach
+<div class="newTagMenu" style='clear:both;'>
+<input type='button' id='showNewRoutineTags{{ $log_entry->routine_id }}' 
+  class='textButton showNewRoutineTags' value='[ Add Tag ]'/>
+<input type='button' id='hideNewRoutineTags{{ $log_entry->routine_id }}' 
+  class='textButton hideNewRoutineTags' value='[ - ]' />
+</div> 
+</div>
+<div id='newRoutineTags{{ $log_entry->routine_id }}' class='newTagList'>
+    @foreach ($tag_types as $tag_type)
+    <form class='newTagForm' method="POST" action="/work-log/public/tag">
+        {{ csrf_field () }}         
+        <input type='hidden' name='newTagType' value='{{ $tag_type->id }}' />
+        <input type='hidden' name='incidentID' value='0' />
+        <input type='hidden' name='routineID' value='{{ $log_entry->routine_id }}' />
+        <input type='hidden' name='logEntryID' value='{{$log_entry->id}}' />
+        <input type='submit' class='textButton' value='{{ $tag_type->name }}' />
+    </form> 
+    @endforeach
+</div>
+
